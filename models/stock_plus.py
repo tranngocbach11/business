4e46d0f7371e774d093d35f51a3688243d86c6f9
tranngocbach11 <tranngocbach0111@gmail.com>
2023-11-02 18:24:04 +0700

    Auto transfer state of purchase order to done

--- models/stock_plus.py
+++ models/stock_plus.py
@@ -13,8 +13,23 @@ class StockPicking(models.Model):
                     purchase_order = self.env['purchase.order'].sudo().search(
                         [('name', '=', sale_order.client_order_ref)], limit=1)
                     if purchase_order:
-                        purchase_order_picking = purchase_order.picking_ids.filtered(lambda p: p.state != ['ready', 'done'])
+                        purchase_order_picking = purchase_order.picking_ids.filtered(lambda p: p.state != ['assigned', 'done'])
                         if purchase_order_picking:
                             purchase_order_picking.write({'state': 'assigned'})
+        return res
+
+    def button_validate(self):
+        res = super(StockPicking, self).button_validate()
+
+        for picking in self:
+            if picking.origin:
+                purchase_order = self.env['purchase.order'].sudo().search([('name', '=', picking.origin)], limit=1)
+                if purchase_order and purchase_order.picking_ids.filtered(lambda p: p.state == 'done'):
+                    sale_order = self.env['sale.order'].sudo().search(
+                        [('client_order_ref', '=', purchase_order.name)], limit=1)
+                    if sale_order:
+                        sale_order_picking = sale_order.picking_ids.filtered(lambda p: p.state != ['assigned', 'done'])
+                        if sale_order_picking:
+                            sale_order_picking.write({'state': 'done'})
 
         return res
